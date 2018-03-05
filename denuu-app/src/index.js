49be3cc18a2c390aa6a98927a5d14d958887e363
Unknown <denis.nossevitch@block64.com>
2018-03-04 22:33:30 -0500

    Tut-based server prep

--- denuu-app/src/index.js
+++ denuu-app/src/index.js
@@ -6,3 +6,10 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
+
+app.use( express.static( `${__dirname}/../build` ) );
+
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
