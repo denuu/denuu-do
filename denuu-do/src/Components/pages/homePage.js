d2bae5cff0f2ac843bb0beece4653fb7e08cee7d
denuu <denis.nossevitch@block64.com>
2018-03-17 00:30:56 -0400

    Teal and cleanup

--- denuu-do/src/Components/pages/homePage.js
+++ denuu-do/src/Components/pages/homePage.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class HomePage extends Component {
   render() {
     return (
-        <div className="wrap">
+        <div className="homePage">
 
             <h1>
                 <b>denuu</b>
             </h1>
-            <h1>
+            <h4>
                 <b>develop with style</b>
-            </h1>
+            </h4>
 
         </div>
     );
