a37fab28ee3fac14706bed86a2c165bb5cc0f1d8
denuu <denis.nossevitch@block64.com>
2018-03-16 23:08:46 -0400

    Finally a website begins...

--- denuu-do/src/App.js
+++ denuu-do/src/App.js
@@ -1,58 +1,22 @@
 import React, { Component } from 'react';
-import { Router, browserHistory, Route, Link } from 'react-router';
-import logo from './logo.svg';
-import './App.css';
-
-const Page = ({ title }) => (
-    <div className="App">
-        <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h2>{title}</h2>
-        </div>
-        <p className="App-intro">
-            This is the {title} page
-        </p>
-        <p>
-            <Link to="/">Home</Link>
-        </p>
-        <p>
-            <Link to="/about">About</Link>
-        </p>
-        <p>
-            <Link to="/settings">Settings</Link>
-        </p>
-    </div>
-);
-
-const Home = (props) => (
-    <Page title="Home"/>
-);
-
-const About = (props) => (
-    <Page title="About"/>
-);
-
-const Settings = (props) => (
-    <Page title="Settings"/>
-);
+import './Assets/css/default.min.css';
+// import logo from './logo.svg';
+import Header from './Components/headerComponent/header';
+import Footer from './Components/footerComponent/footer';
+import HomePage from './Components/pages/homePage';
 
 class App extends Component {
   render() {
     return (
-        <Router history={browserHistory}>
-            <Route path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/settings" component={Settings}/>
-        </Router>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
+      <div className="App">
+
+          <Header />
+
+            <HomePage />
+
+          <Footer />
+
+      </div>
     );
   }
 }
---
+++ denuu-do/src/Components/pages/homePage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+        <div className="wrap">
+
+            <h1>
+                Home Page Content
+            </h1>
+
+            <p>
+                I'm dummy text lalala. I'm dummy text lalala. I'm dummy text lalala. v I'm dummy text lalala.I'm dummy text lalala.I'm dummy text lalala. I'm dummy text lalala. I'm dummy text lalala. I'm dummy text lalala.
+            </p>
+
+        </div>
+    );
+  }
+}
+
+export default HomePage;
